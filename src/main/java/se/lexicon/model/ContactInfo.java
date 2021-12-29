14ed0c9119c2dd5b970801079fdd5e8175467957
Mehrdad Javan <mehrdad.javan@adapta.se>
2021-12-29 09:50:56 +0100

    add contact information blueprint class

---
+++ src/main/java/se/lexicon/model/ContactInfo.java
@@ -0,0 +1,49 @@
+package se.lexicon.model;
+
+import java.util.UUID;
+
+public class ContactInfo {
+  private String id;
+  private String email;
+  private String phone;
+
+  public ContactInfo(String id, String email, String phone) {
+    if (id == null){
+      throw new RuntimeException("id was null");
+    }
+    this.id = id;
+    this.email = email;
+    this.phone = phone;
+  }
+
+  public ContactInfo(String email, String phone) {
+    this(UUID.randomUUID().toString(),email,phone);
+
+  }
+
+  public ContactInfo() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    //
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    //
+    this.phone = phone;
+  }
+}
