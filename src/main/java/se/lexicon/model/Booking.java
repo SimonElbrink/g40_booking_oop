87788317a18d943d3572cfd6a977971f920450e1
Mehrdad Javan <mehrdad.javan@adapta.se>
2021-12-29 10:40:58 +0100

    add booking blueprint class

---
+++ src/main/java/se/lexicon/model/Booking.java
@@ -0,0 +1,115 @@
+package se.lexicon.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Booking {
+
+  private String id;
+  private LocalDateTime dateTime;
+  private double price;
+  private String administrator;
+  private String vaccineId;
+  private boolean vacant;
+  private Patient patient;
+  private Premises premises;
+
+  public Booking(String id,
+                 LocalDateTime dateTime,
+                 double price,
+                 String administrator,
+                 String vaccineId,
+                 boolean vacant,
+                 Patient patient,
+                 Premises premises) {
+    if (id == null) throw new RuntimeException("id was null");
+    this.id = id;
+    setDateTime(dateTime);
+    setPrice(price);
+    setAdministrator(administrator);
+    setVaccineId(vaccineId);
+    setVacant(vacant);
+    setPatient(patient);
+    setPremises(premises);
+  }
+
+  public Booking(LocalDateTime dateTime,
+                 double price,
+                 String administrator,
+                 String vaccineId,
+                 Patient patient,
+                 Premises premises) {
+
+    this(UUID.randomUUID().toString(), dateTime, price, administrator, vaccineId,true, patient, premises);
+  }
+
+  Booking() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public LocalDateTime getDateTime() {
+    return dateTime;
+  }
+
+  public void setDateTime(LocalDateTime dateTime) {
+    if (dateTime == null) throw new IllegalArgumentException("Parameter dateTime was null");
+    this.dateTime = dateTime;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String getAdministrator() {
+    return administrator;
+  }
+
+  public void setAdministrator(String administrator) {
+    this.administrator = administrator;
+  }
+
+  public String getVaccineId() {
+    return vaccineId;
+  }
+
+  public void setVaccineId(String vaccineId) {
+    if (vaccineId == null) throw new IllegalArgumentException("vaccineId was null");
+    this.vaccineId = vaccineId;
+  }
+
+  public boolean isVacant() {
+    return vacant;
+  }
+
+  private void setVacant(boolean vacant) {
+    this.vacant = vacant;
+  }
+
+  public Patient getPatient() {
+    return patient;
+  }
+
+  public void setPatient(Patient patient) {
+    this.patient = patient;
+  }
+
+  public Premises getPremises() {
+    return premises;
+  }
+
+  public void setPremises(Premises premises) {
+    this.premises = premises;
+  }
+
+  public String bookingData(){
+    return "Booking: " + "id: " + id + "dateTime: " + dateTime + "vaccineId: " + vaccineId+ "vacant: " + vacant;
+  }
+
+}
