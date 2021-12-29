1ccfb3e3d1b46d2eb1e33c7ee6907944662fab3c
Mehrdad Javan <mehrdad.javan@adapta.se>
2021-12-29 10:09:20 +0100

    add user credentials blueprint class

---
+++ src/main/java/se/lexicon/model/UserCredentials.java
@@ -0,0 +1,64 @@
+package se.lexicon.model;
+
+import java.util.UUID;
+
+public class UserCredentials {
+
+  /*
+  - id:String
+  - username:String
+  - password:String
+  - role:String
+  */
+
+  private String id;
+  private String username;
+  private String password;
+  private String role;
+
+  public UserCredentials(String id, String username, String password, String role) {
+    if (id == null) throw new RuntimeException("id was null");
+    this.id = id;
+    setUsername(username);
+    setPassword(password);
+    setRole(role);
+  }
+
+  public UserCredentials(String username, String password, String role) {
+    this(UUID.randomUUID().toString(), username,password,role);
+  }
+
+  public UserCredentials() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    if (username == null) throw new IllegalArgumentException("Username was null");
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    if (password == null) throw new IllegalArgumentException("Password was null");
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    if (role == null) throw new IllegalArgumentException("Role was null");
+    this.role = role;
+  }
+}
