72b19758d428e089ddbd0acf676ad4b7632970ea
Mehrdad Javan <mehrdad.javan@adapta.se>
2021-12-29 09:42:45 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/model/Address.java
@@ -0,0 +1,68 @@
+package se.lexicon.model;
+
+import java.util.UUID;
+
+public class Address {
+  // Fields
+  private String id;
+  private String streetAddress;
+  private String zipCode;
+  private String city;
+
+  public Address(String id, String streetAddress, String zipCode, String city) {
+    if (id == null) {
+      throw new RuntimeException("id was null");
+    }
+    this.id = id;
+    setStreetAddress(streetAddress);
+    setZipCode(zipCode);
+    setCity(city);
+  }
+
+  public Address(String streetAddress, String zipCode, String city) {
+    this(UUID.randomUUID().toString(), streetAddress, zipCode, city);
+  }
+
+  Address() {
+
+  }
+
+  // setters & getters
+  public String getId() {
+    return id;
+  }
+
+
+  public String getStreetAddress() {
+    return streetAddress;
+  }
+
+  public void setStreetAddress(String streetAddress) {
+    if (streetAddress == null) {
+      throw new IllegalArgumentException("Parameter: String streetAddress was null");
+    }
+    this.streetAddress = streetAddress;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    if (zipCode == null) {
+      throw new IllegalArgumentException("Parameter: String zipCode was null");
+    }
+    this.zipCode = zipCode;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    if (city == null) {
+      throw new IllegalArgumentException("Parameter: String city was null");
+    }
+    this.city = city;
+  }
+}
