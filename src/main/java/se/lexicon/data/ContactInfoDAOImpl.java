47a85e8909fc5801b841b29e88e407e97d3903b0
Simon Elbrink <simon.elbrink@gmail.com>
2022-02-03 15:41:53 +0100

    DAO ContactInfoDAO Implemented.

---
+++ src/main/java/se/lexicon/data/ContactInfoDAOImpl.java
@@ -0,0 +1,62 @@
+package se.lexicon.data;
+
+import se.lexicon.data.interfaces.ContactInfoDAO;
+import se.lexicon.io.JsonManager;
+import se.lexicon.io.URLConstants;
+import se.lexicon.model.ContactInfo;
+
+import java.io.File;
+import java.util.*;
+
+public class ContactInfoDAOImpl implements ContactInfoDAO {
+
+    private static ContactInfoDAOImpl INSTANCE;
+
+    public static ContactInfoDAOImpl getInstance(){
+        if (INSTANCE == null) INSTANCE = new ContactInfoDAOImpl(null);
+        return INSTANCE;
+    }
+
+
+    public ContactInfoDAOImpl(Set<ContactInfo> contactInfoSet) {
+        if(contactInfoSet == null){
+            this.contactInfoSet = new HashSet<>(JsonManager.getInstance().deserializeFromJson(new File(URLConstants.CONTACT_INFO_JSON), ContactInfo.class));
+        }else{
+            this.contactInfoSet = contactInfoSet;
+        }
+    }
+    private final Set<ContactInfo> contactInfoSet;
+
+
+    @Override
+    public Optional<ContactInfo> findByEmail(String email) {
+        return contactInfoSet.stream()
+                .filter(contactInfo -> contactInfo.getEmail().equalsIgnoreCase(email))
+                .findFirst();
+    }
+
+    @Override
+    public ContactInfo create(ContactInfo contactInfo) {
+        if (contactInfo == null) throw new IllegalArgumentException("ContactInfo was null.");
+        if (contactInfo.getId() == null) throw new IllegalArgumentException("ContactInfo.id was null.");
+
+        return contactInfoSet.add(contactInfo) ? contactInfo : null;
+    }
+
+    @Override
+    public List<ContactInfo> findAll() {
+        return new ArrayList<>(contactInfoSet);
+    }
+
+    @Override
+    public Optional<ContactInfo> findById(String id) {
+        return contactInfoSet.stream()
+                .filter(contactInfo -> contactInfo.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return contactInfoSet.removeIf(contactInfo -> contactInfo.getId().equals(id));
+    }
+}
---
+++ src/main/java/se/lexicon/data/interfaces/ContactInfoDAO.java
@@ -0,0 +1,10 @@
+package se.lexicon.data.interfaces;
+
+import se.lexicon.model.ContactInfo;
+
+import java.util.Optional;
+
+public interface ContactInfoDAO extends GenericCRUD<ContactInfo, String>{
+
+    Optional<ContactInfo> findByEmail(String email);
+}
